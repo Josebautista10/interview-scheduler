f249d7f317ff65de0e6f1562d1e63c4dc2e65f55
Jose Bautista <Cooldj1020@gmail.com>
2021-10-04 18:31:57 -0600

    implemented error messages

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Show from './Show';
 import Empty from './Empty';
 import Status from './Status';
 import Confirm from './Confirm';
+import Error from './Error';
 import Form from './Form';
 
 import useVisualMode from "hooks/useVisualMode";
@@ -18,6 +19,8 @@ const SAVING = 'SAVING';
 const DELETING = 'DELETING';
 const CONFIRM = 'CONFIRM';
 const EDIT = 'EDIT';
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
 
 
@@ -34,13 +37,15 @@ const Appointment = (props) => {
       transition(SAVING)
       props.bookInterview(props.id, interview)
         .then(() => transition(SHOW))
+        .catch(() => transition(ERROR_SAVE, true));
     }
 
     const deleteAppt = (id) => {
-      transition(DELETING)
+      transition(DELETING, true)
     props
       .cancelInterview(id)
       .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true));
     }
 
   return (
@@ -80,6 +85,18 @@ const Appointment = (props) => {
           onCancel={() => back()}
           save={save}
           onConfirm={() => deleteAppt(props.id)}
+        />
+      )}
+      {mode === ERROR_SAVE && (
+        <Error 
+          message="Could not make this appointment"
+          onClose={() => back()} 
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error 
+          message="Could not cancel appointment"
+          onClose={() => back()} 
         />
       )}
     </article>
